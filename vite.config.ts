f2035351adeba1cef76a9ee7cea410b75710649e
kost1k <kostik048@gmail.com>
2022-08-24 02:34:00 +0300

    fix urls for gh page

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import path from 'path';
+import copy from 'rollup-plugin-copy';
 import { defineConfig } from 'vite';
 import { book } from 'vite-plugin-book/vite';
 
@@ -8,6 +10,15 @@ export default defineConfig({
             name: 'PHP course',
             path: 'docs',
             repo: 'https://github.com/intaro/php-course',
+        }),
+        copy({
+            targets: [
+                {
+                    src: path.resolve(__dirname, '404.html'),
+                    dest: path.resolve(__dirname, './dist'),
+                },
+            ],
+            hook: 'writeBundle',
         }),
     ],
 });
